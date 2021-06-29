efbcb0d538d2156181e8430d8a78dba750654551
npickus <nick.m.pickus@live.com>
2021-06-29 06:59:42 -0500

    Update ColumnAgingData.cs

--- ExcelController/Model/ColumnAgingData.cs
+++ ExcelController/Model/ColumnAgingData.cs
@@ -11,6 +11,7 @@ namespace ExcelController.Model
         public int WorkItemId { get; set; }
         public string WorkItemType { get; set; }
         public string Title { get; set; }
+        public string AreaPath { get; set; }
         public string Url { get; set; }
         public string CurrentColumn { get; set; }
         public string ColumnEntryDate { get; set; }
